f677c04ed86f02a7e0e055fbc293da2a7851a7c1
Israel Curiel <israelcuriel597@hotmail.com>
2024-05-27 23:54:36 -0600

    cambio de modelo

--- Api.Microservice.Autor/Modelo/AutorLibro.cs
+++ Api.Microservice.Autor/Modelo/AutorLibro.cs
@@ -2,11 +2,6 @@
 {
     public class AutorLibro
     {
-        public int AutorLibroId { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public DateTime? FechaNacimiento { get; set; }
-        public ICollection<GradoAcademico> GradoAcademicos { get; set; }
-        public string AutorLibroGuid {  get; set; }
+        
     }
 }
